13df036f99e7b84c21a7132e9d084936fe9461e0
AdamP-Iot <adam.picka2@seznam.cz>
2026-04-03 20:50:43 +0200

    "Basic structure of Group class"

--- src/main/java/cz/uhk/graphed/model/Group.java
+++ src/main/java/cz/uhk/graphed/model/Group.java
@@ -1,15 +1,43 @@
 package cz.uhk.graphed.model;
 
 import java.awt.*;
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class Group extends AbstractGraphicObject{
+    private ArrayList<AbstractGraphicObject> objects;
+
+    public Group (Point position, Color color){
+        super(position,color);
+        objects = new ArrayList<>();
+    };
+    public Group(){};
+
+    public void addObject(AbstractGraphicObject object) {
+        objects.add(object);
+    }
+
     @Override
     public void draw(Graphics g) {
+        for(AbstractGraphicObject object : objects){
+            object.draw(g);
+        }
+    }
 
+    @Override
+    public void move(int dx, int dy) {
+        for(AbstractGraphicObject object : objects){
+            object.move(dx,dy);
+        }
     }
 
     @Override
     public boolean contains(Point p) {
+        for(AbstractGraphicObject object : objects) {
+            if(object.contains(p)){
+                return true;
+            }
+        }
         return false;
     }
 }
